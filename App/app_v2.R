a1b6cbdba36c513d7942e0162209f8df3e9b54bb
eltonjohnfanboy <adarsh0x1702@gmail.com>
2023-07-17 09:34:25 +0200

    Webapp style modification

--- App/app_v2.R
+++ App/app_v2.R
@@ -6,18 +6,22 @@ library(ggplot2)
 library(lubridate)
 library(dplyr)
 library(readr)
-library (plotly)
+library(plotly)
+library(bslib)
 
 
 # Set the working directory
 setwd("C:/Users/adars/OneDrive/Escritorio/ProjecteLolShiny/Data")
+
 
 # Read data
 data_lec <- read_csv("PlayerStatesTrajectory.csv")
 data_traj <- read_csv("PlayersTrajectoyData.csv")
 
 # Define ui
-ui <- fluidPage(theme = shinytheme("superhero"),
+ui <- fluidPage(
+                includeCSS("C:/Users/adars/OneDrive/Escritorio/ProjecteLolShiny/www/style.css"),
+                theme = shinythemes::shinytheme("slate"),
                 navbarPage(
                   "LeagueStats",
                   tabPanel("Player stats",
@@ -28,8 +32,7 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                              uiOutput("yearSelection")
                            ),
                   mainPanel(
-                        textOutput("player_name"),
-                        textOutput("year_team")
+                        uiOutput("player_info_box")
                           ),
                   mainPanel(
                     plotlyOutput("trajectoryPlot")
@@ -57,14 +60,7 @@ server <- function(input, output, session){
   # Display player's name
   output$player_name <- renderText({
     player_data <- selected_data()
-    paste("Official name: ", unique(player_data$Player))
-  })
-  
-  # Display year and team information
-  output$year_team <- renderText({
-    player_data <- selected_data()
-    mean_kda <- mean(player_data$KDA)
-    paste("Year:", unique(player_data$Year), " | KDA:", mean_kda)
+    paste("Selected player: ", unique(player_data$Player))
   })
   
   # Display trajectory plot
@@ -78,14 +74,42 @@ server <- function(input, output, session){
     )
     mdfr <- melt(dfr, measure.vars = c("start.date", "end.date"))
     plot_traj <- ggplot(mdfr, aes(value, name)) + 
-      geom_line(size = 6, colour = "skyblue") +
-      xlab(NULL) + 
-      ylab(NULL)
+                geom_line(size = 6, colour = "#6a6e70") +
+                xlab(NULL) + 
+                ylab(NULL) +
+                ggtitle("Player Trajectory:") +
+                theme_dark() +       # Use a minimalistic theme
+                theme(
+                plot.title = element_text(color="white"),
+                panel.background = element_rect(fill = "black"),     # Set panel background to black
+                plot.background = element_rect(fill = "black")       # Set plot background to black
+              )
     ggplotly(plot_traj)
-  }) 
-  
+  })
+
+  # Player information
+  output$player_info_box <- renderUI({
+    selected_player <- selected_data()
+      # Create a list to store the static boxes
+    info_boxes <- lapply(seq_len(nrow(selected_player)), function(i) {
+      div(
+        class = "info-box",
+        h3(paste("Player:", input$player)),
+        p(paste("Team:", selected_player$Team[i])),
+        p(paste("Position:", selected_player$Team[i])),
+        p(paste("Goals:", selected_player$KDA[i]))
+      )
+    })
+    
+    box_columns <- lapply(info_boxes, function(box) {
+      column(width = 4, box)  # Adjust the width as needed
+    })
+    
+    # Return the boxes wrapped in a fluidRow
+    fluidRow(box_columns)
+  })
+
 }
 
 # Run the Shiny app
 shinyApp(ui, server)
-
