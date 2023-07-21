9b42ab4339c72928c3319cb2758f841b5c04a400
eltonjohnfanboy <adarsh0x1702@gmail.com>
2023-07-21 17:41:25 +0200

    Scatter plot added

--- App/app_v2.R
+++ App/app_v2.R
@@ -8,6 +8,7 @@ library(dplyr)
 library(readr)
 library(plotly)
 library(bslib)
+library(forcats)
 
 
 # Set the working directory
@@ -16,6 +17,7 @@ setwd("C:/Users/adars/OneDrive/Escritorio/ProjecteLolShiny/Data")
 
 # Read data
 data_lec <- read_csv("PlayerStats.csv")
+data_indiv <- read_csv("PlayerStatsGeneral.csv")
 data_traj <- read_csv("PlayersTrajectoyData.csv")
 
 # Define ui
@@ -28,16 +30,23 @@ ui <- fluidPage(
                            sidebarPanel(
                                   tags$h3("Search Bar"),
                                   selectInput("player", "Select Player:", choices = unique(data_lec$Player),
-                                              width = "100%"),
+                                              width = "100%", selected = "Elyoya"),
                                   uiOutput("yearSelection"),
                                   uiOutput("eventSelection")
                             ),
-                  mainPanel(
-                        uiOutput("player_info_box")
-                          ),
-                  mainPanel(
-                    plotlyOutput("trajectoryPlot")
-                          )
+                  ),
+                  fluidRow(
+                          column(4, mainPanel(
+                                uiOutput("player_info_box")
+                                  )),
+                  ),
+                  fluidRow(
+                          column(6, mainPanel(
+                            plotlyOutput("trajectoryPlot")
+                                  )),
+                          column(6, mainPanel(
+                            plotlyOutput("dthKPscatterPlot")
+                                  )),
                   ),
                   tabPanel("Team stats", "Under construction 👷"),
                   tabPanel("Team comparison", "Under construction 👷"),
@@ -54,18 +63,18 @@ ui <- fluidPage(
 server <- function(input, output, session){
   
   selected_player <- reactive({
-    filter(data_lec, Player == input$player) 
+    filter(data_lec, Player == input$player)
   })
   
   output$yearSelection <- renderUI({selectInput("year", "Select Year:", choices = unique(selected_player()$Year),
-                                                width = "100%")})
+                                                width = "100%", selected = "2021")})
   
 selected_player_year <- reactive({
     filter(data_lec, Player == input$player & Year == input$year) 
   })
 
   output$eventSelection <- renderUI({selectInput("event", "Select event:", choices = unique(selected_player_year()$Event),
-                                                width = "100%")})
+                                                width = "100%", selected = "MSI 2021")})
   
   selected_data <- reactive({
     filter(data_lec, Player == input$player & Year == input$year & Event == input$event) 
@@ -78,11 +87,11 @@ selected_player_year <- reactive({
   })
   
   # Display trajectory plot
-  
   output$trajectoryPlot <- renderPlotly({
     data_aux <- data_traj %>% filter(Player == input$player)
     dfr <- data.frame(
-    name = factor(c(data_aux$`Teams trajectory`), levels = data_aux$`Teams trajectory`),
+    #name = factor(c(data_aux$`Teams trajectory`), levels = data_aux$`Teams trajectory`),
+    name = data_aux$`Teams trajectory` %>% fct_inorder(),
     start.date = as.Date(c(data_aux$start_date)),
     end.date = as.Date(c(data_aux$end_date))
     )
@@ -101,6 +110,30 @@ selected_player_year <- reactive({
     ggplotly(plot_traj)
   })
 
+  # DEATH% and KP scatter
+  output$dthKPscatterPlot <- renderPlotly({
+        data_event <- data_indiv %>% filter(Event == input$event & Year == input$year)
+        data_aux <- data_indiv %>% filter(Player == input$player & Event == input$event & Year == input$year)
+        p <- ggplot(data_event, aes(x = KP, y = `DTH%`)) + 
+                    geom_point(data = data_aux, color = "red", size = 3, aes(text = paste("Player: ", Player, "<br>", "Position: ", Position))) +
+                    geom_point(data = data_event[data_event$Player != input$player, ], color = "gray", size = 3, aes(text = paste("Player: ", Player, "<br>", "Position: ", Position))) +
+                    #geom_text(data = data_aux, aes(label = Player), hjust = 0, vjust = 10, size = 4, color = "red") +
+                    labs(title = "Relation KP and DTH%", x = "Kill participation (%)", y = "Team's death participation (%)") + 
+                    theme_dark() +
+                    theme(
+                    plot.title = element_text(color="white"),
+                    axis.title.x = element_text(color = "white"),
+                    axis.title.y = element_text(color = "white"),
+                    panel.background = element_rect(fill = "black"),     # Set panel background to black
+                    plot.background = element_rect(fill = "black")       # Set plot background to black
+                    )
+
+        ggplotly(p)
+
+  })
+
+  
+
   # Player information
   output$player_info_box <- renderUI({
     selected_player <- selected_data()
@@ -108,7 +141,7 @@ selected_player_year <- reactive({
     info_boxes <- lapply(seq_len(nrow(selected_player)), function(i) {
       div(
         class = "info-box",
-        h3(paste("Player:", selected_player$"Player original name")),
+        h3(paste("Player:", selected_player$"Player original name"[i])),
         p(paste("Team:", selected_player$Team[i])),
         p(paste("Position:", selected_player$Position[i])),
         p(paste("KDA:", selected_player$KDA[i]))
--- App/test.R
+++ App/test.R
@@ -5,23 +5,7 @@ library(dplyr)
 library(readr)
 
 setwd("C:/Users/adars/OneDrive/Escritorio/ProjecteLolShiny/Data")
-data_lec <- read_csv("PlayersTrajectoyData.csv")
-#data_lec$converted_dates <- as.Date(data_lec$converted_dates)
-
-
-
-data_aux <- data_lec %>% filter(Player == "113")
-
-
-
-dfr <- data.frame(
-  name = factor(c(data_aux$`Teams trajectory`), levels = data_aux$`Teams trajectory`),
-  start.date = as.Date(c(data_aux$start_date)),
-  end.date = as.Date(c(data_aux$end_date))
-)
-
-mdfr <- melt(dfr, measure.vars = c("start.date", "end.date"))
-ggplot(mdfr, aes(value, name)) + 
-  geom_line(size = 6, colour = "skyblue") +
-  xlab(NULL) + 
-  ylab(NULL)
+data_lec <- read_csv("PlayerStats.csv")
+data_lec$KP <- as.numeric(gsub("%", "", data_lec$KP))
+data_lec$`DTH%` <- as.numeric(gsub("%", "", data_lec$`DTH%`))
+ggplot(data_lec, aes(x = KP, y = `DTH%`)) + geom_point(color = "blue", size = 3)
